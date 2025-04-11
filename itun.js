5cd9dc60217ffeaaf4355130c101d7826b9033e6
liulongxuan <115394664+liulongxuan@users.noreply.github.com>
2025-04-11 14:18:18 +0800

    Update itun.js

--- itun.js
+++ itun.js
@@ -297,17 +297,15 @@ for (const i in list) {
   //识别数据，处理到期时间或永久，多重购买
   if (list[i].cm.indexOf('sjbla') != -1) {  sjbl = [Object.assign({}, receiptdata, {  "expires_date":"2099-09-09 09:09:09 Etc/GMT", "expires_date_pst":"2099-09-09 06:06:06 America/Los_Angeles", "is_in_intro_offer_period":"false", "web_order_line_item_id":"490000123456789", "expires_date_ms":"4092599349000",  })];} else if (list[i].cm.indexOf('sjblb') != -1) {  sjbl = [(receiptdata)];} else if (list[i].cm.indexOf('sjblc') != -1) {  sjbl = [];  } else if (list[i].cm.indexOf('sjbld') != -1) {  sjbl = [Object.assign({}, receiptdata, {  "product_id":(list[i].ids)}), Object.assign({}, receiptdata, {  "expires_date":"2099-09-09 09:09:09 Etc/GMT", "expires_date_pst":"2099-09-09 06:06:06 America/Los_Angeles", "is_in_intro_offer_period":"false", "web_order_line_item_id":"490000123456789", "expires_date_ms":"4092599349000", "product_id":(list[i].id)  })];}
   //常用数据核心(有到期时间)
-  if (list[i].hx.indexOf('hxpda') != -1) {  chxm1023["receipt"]["in_app"] = (sjbl); liulongxuan["latest_receipt_info"] = (sjbl); liulongxuan["pending_renewal_info"] = [{  "product_id" : (list[i].id), "original_transaction_id" : "490001314520000", "auto_renew_product_id" : (list[i].id), "auto_renew_status" : "1"  }]; chxm1023["latest_receipt"] = (list[i].latest);  }
+  if (list[i].hx.indexOf('hxpda') != -1) {  liulongxuan["receipt"]["in_app"] = (sjbl); liulongxuan["latest_receipt_info"] = (sjbl); liulongxuan["pending_renewal_info"] = [{  "product_id" : (list[i].id), "original_transaction_id" : "490001314520000", "auto_renew_product_id" : (list[i].id), "auto_renew_status" : "1"  }]; chxm1023["latest_receipt"] = (list[i].latest);  }
   //永久数据核心(无到期时间)
-  if (list[i].hx.indexOf('hxpdb') != -1) {  chxm1023["receipt"]["in_app"] = (sjbl);  }
+  if (list[i].hx.indexOf('hxpdb') != -1) {  liulongxuan["receipt"]["in_app"] = (sjbl);  }
   //新核心-类似解压小橙子
-  if (list[i].hx.indexOf('hxpdc') != -1) {  chxm1023["receipt"]["in_app"] = (sjbl);  }
+  if (list[i].hx.indexOf('hxpdc') != -1) {  liulongxuan["receipt"]["in_app"] = (sjbl);  }
   //通用数据(可有可无)
   var commondata = {  "request_date": "2023-09-09 16:06:27 Etc/GMT", "request_date_pst": "2023-09-09 06:06:27 America/Los_Angeles", "request_date_ms": "1694273635000", "original_purchase_date": "2023-09-09 16:00:00 Etc/GMT", "original_purchase_date_pst": "2023-09-09 06:00:00 America/Los_Angeles", "original_purchase_date_ms": "1694273430000", "receipt_creation_date": "2023-09-09 16:06:26 Etc/GMT", "receipt_creation_date_pst": "2023-09-09 06:06:26 America/Los_Angeles", "receipt_creation_date_ms": "1694273634000", "original_application_version": list[i].version}; chxm1023["receipt"] = Object.assign({}, chxm1023["receipt"], commondata );
   //判断是否需要加入版本号(可有可无)
   if(list[i] && list[i].version && list[i].version.trim() !== '') {  liulongxuan["receipt"]["original_application_version"] = list[i].version;  }
-  chxm1023["Telegram"] = "https://t.me/chxm1023";
-  chxm1023["warning"] = "仅供学习，禁止转载或售卖";
   break;
   }
 }
