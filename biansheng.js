ef12f0485f5a6d573b21d247d6f09c47dbea8414
liulongxuan <115394664+liulongxuan@users.noreply.github.com>
2025-04-12 12:14:37 +0800

    Create biansheng.js

---
+++ biansheng.js
@@ -0,0 +1,23 @@
+[rewrite_local] 
+
+^https:\/\/appss\.baomingding\.com\/user\/getUserInfo url script-response-body https://raw.githubusercontent.com/liulongxuan/liu/refs/heads/main/biansheng.js
+
+[MITM]
+hostname = appss.baomingding.com
+
+
+ */
+ 
+var long = JSON['parse']($response['body']);
+long = {
+   "groupType" : "TYPE_ONE",
+        "vipType" : "VIP",
+        "autoPay" : "YES"
+         "type" = "VIP";
+    "freeFlag" = "YES";
+  "vipExpireDays" = 99999999999;
+   "vipExpireTime" = "2999-01-01 00:00:00";
+};
+$done({
+    'body': JSON['stringify'](long)
+});;
