1c4ff8203d10ec24fed11f87c3b6dba0a3257029
liulongxuan <115394664+liulongxuan@users.noreply.github.com>
2025-04-12 12:31:10 +0800

    Update biansheng.js

--- biansheng.js
+++ biansheng.js
@@ -8,16 +8,22 @@ hostname = appss.baomingding.com
 
  */
  
-var long = JSON['parse']($response['body']);
-long = {
-   "groupType" : "TYPE_ONE",
+var long = $response.body;
+var obj =  JSON.parse(long);
+
+if($request.url.indexOf("/app/account/getAccountInfo") != -1) {
+    obj.result.type = "VIP";
+    obj.result.freeFlag = "YES";
+    obj.result.vipExpireDays = 99999999999;
+    obj.result.vipExpireTime = "2999-01-01 00:00:00";
+    obj.result.vipGroupInfos = [
+       {
+        "groupType" : "TYPE_ONE",
         "vipType" : "VIP",
         "autoPay" : "YES"
-         "type" =:"VIP";
-    "freeFlag" : "YES";
-  "vipExpireDays" : 99999999999;
-   "vipExpireTime" : "2999-01-01 00:00:00";
-};
-$done({
-    'body': JSON['stringify'](long)
-});;
+      }
+    ];
+}
+
+aFengYe = JSON.stringify(obj);
+$done(long);
